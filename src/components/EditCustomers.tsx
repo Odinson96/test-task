192301cfd7c37fd91b8dbc7103cfaed7f6994117
Odinson96 <egk090@gmail.com>
2023-03-30 05:18:30 +0400

    initial commit

--- src/components/EditCustomers.tsx
+++ src/components/EditCustomers.tsx
@@ -57,7 +57,7 @@ export function EditCustomers({ data, onClick, comeBack }: EditCustomers) {
     });
     setTimeout(() => {
       comeBack();
-    });
+    }, 1000);
   };
 
   const changeType = () => {
